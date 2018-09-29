162cc2110229393fec371ba8d8fd2bed53f4787f
João Pedro Almeida <joao.henriques.almeida@tecnico.ulisboa.pt>
2018-09-29 18:57:21 +0100

    Finishes LAB-02

--- Laboratórios/LAB-02/script.sh
+++ Laboratórios/LAB-02/script.sh
@@ -1,1 +1,7 @@
-#!bin/bash
+#!/bin/bash
+for input in inputs/*.txt
+do
+	echo ==== ${input} ====
+	echo Line count: $(wc -l < "${input}")
+	./CircuitRouter-SeqSolver < "${input}" | grep "Paths routed" 
+done
