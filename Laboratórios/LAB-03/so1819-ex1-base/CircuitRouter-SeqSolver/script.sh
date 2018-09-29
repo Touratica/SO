af78515a3e803c3b16c02bf95a94fcf3d92bd39c
João Pedro Almeida <joao.henriques.almeida@tecnico.ulisboa.pt>
2018-09-29 23:52:58 +0100

    Updates script.sh in LAB-03

--- Laboratórios/LAB-03/so1819-ex1-base/CircuitRouter-SeqSolver/script.sh
+++ Laboratórios/LAB-03/so1819-ex1-base/CircuitRouter-SeqSolver/script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+myApp=CircuitRouter-SeqSolver
+if [ ! -f ${myApp} ]; then
+	echo "Program ${myApp} does not exit"
+	exit 2
+fi
 for input in inputs/*.txt
 do
 	echo ==== ${input} ====
 	echo Line count: $(wc -l < "${input}")
-	./CircuitRouter-SeqSolver < "${input}" | grep "Paths routed" 
+	./${myApp} < "${input}" | grep "Paths routed" 
 done
