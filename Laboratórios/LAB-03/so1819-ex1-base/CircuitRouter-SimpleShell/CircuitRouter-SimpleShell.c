ba2c5b6fef386ae32302d49c8c111058b2b20067
Nikoletta Matsur <nikoletta.matsur@tecnico.ulisboa.pt>
2018-10-06 13:43:58 +0100

    Makes changes to CircuuitRouter-SimpleShell

--- Laboratórios/LAB-03/so1819-ex1-base/CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
+++ Laboratórios/LAB-03/so1819-ex1-base/CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+#include "CircuitRouter-SimpleShell.h"
+
 
 #define MAX_COMMAND_LENGTH 6
 
 int main(int argc, char** argv) {
 	char command[MAX_COMMAND_LENGTH];
-	fprintf(stdout, "$ ");
-	fgets(command, sizeof(command), stdin);
-	if (!strcmp(command, "run\n")) {
-		fprintf(stdout, "Tada!!!!\n");
-		return 0;
+	char c;
+	int i;
+	const long long maxchildren;
+	const char* errstr;
+
+	if (argc==2){
+		for (i=0; (c = argv[1][i]) != '\0'; i++){
+			if (c < '0' || c > '9') {
+				fprintf(stderr, "%s: invalid argument\n", argv[0]);
+				displayUsage(argv[0]);
+			}
+		}
+		maxchildren = strtonum(argv[1], 0, LLONG_MAX, &errstr);
+		if (errstr) {
+			errx(1, "%s: not a valid MAXCHILDREN number", argv[0]);
+		}
 	}
-	return 1;
+
+	fprintf(stdout, "$ ");
+	//fgets(command, sizeof(command), stdin);
+	for ((i = 0; c=getchar())!=' ' || c='\n'; i++) {
+		command[i]=c;
+	}
+	return 0;
+}
+
+static void displayUsage (const char* appName){
+	printf("Usage: %s [MAXCHILDREN]\n", argv[0]);
+	printf("\tMAXCHILDREN defines the limit of child processes allowed\n");
+	exit(1);
 }
