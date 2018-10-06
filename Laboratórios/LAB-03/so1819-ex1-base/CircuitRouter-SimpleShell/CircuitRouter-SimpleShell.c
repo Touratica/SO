b0ebe0708c0bb2d72a0c3f9bc88856bf7c1fdc1b
João Pedro Almeida <joao.henriques.almeida@tecnico.ulisboa.pt>
2018-10-06 17:57:10 +0100

    Completes SimpleShell without parallel processing

--- Laboratórios/LAB-03/so1819-ex1-base/CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
+++ Laboratórios/LAB-03/so1819-ex1-base/CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
@@ -9,13 +9,16 @@
 #define TRUE 1
 
 int main(int argc, char** argv) {
-	char command[MAX_BUFFER];
+	char buffer[MAX_BUFFER], pathbuffer[MAX_BUFFER], inputbuffer[MAX_BUFFER];
 	char c;
 	int i;
 	long long maxchildren;
 
-
-	if (argc==2){
+	if (argc > 2) {
+		fprintf(stderr, "%s: too many arguments\n", argv[0]);
+		displayUsage(argv[0]);
+	}
+	else if (argc==2){
 		for (i=0; (c = argv[1][i]) != '\0'; i++){
 			if (c < '0' || c > '9') {
 				fprintf(stderr, "%s: invalid argument\n", argv[0]);
@@ -33,40 +36,51 @@ int main(int argc, char** argv) {
 
 	while (TRUE){
 		fprintf(stdout, "$ ");
-		for (i = 0; (c = getchar()) != ' ' || c != '\n'; i++) {
-			command[i]=c;
+		for (i = 0; (c = getchar()) != ' ' && c != '\n'; i++) {
+			buffer[i]=c; // gets command
 		}
-		command[i+1] = '\0';
-		if (!strcmp(command, "run")) {
-			if (c == ' ') {	
+		buffer[i] = '\0';
+
+		if (!strcmp(buffer, "run")) {
+			if (c == ' ') {
 				for (i = 0; (c = getchar()) != '\n'; i++) {
-					command[i]=c;
+					buffer[i] = c; // gets inputfile
 				}
-				command[i+1] = '\0';  
-				if (access(command, R_OK)) {
-					fprintf(stderr, "run: input file is unreadable");
+				buffer[i] = '\0';
+				if (getcwd(pathbuffer, sizeof(pathbuffer)) != NULL) {
+					strcpy(inputbuffer, pathbuffer);
+					strcat(inputbuffer, "/");
+					strcat(inputbuffer, buffer);
 				}
 				else {
-					// TODO execute CircuitRouter-SeqSolver
+					// TODO error inputfile path is too long
+				}
+				if (access(inputbuffer, R_OK)) {
+					fprintf(stderr, "run: input file is unreadable\n");
+				}
+				else {
+					strcat(pathbuffer, "/../CircuitRouter-SeqSolver/CircuitRouter-SeqSolver");
+					execl(pathbuffer, "./CircuitRouter-SeqSolver", inputbuffer, NULL);
 				}
 			}
 			else {
-				fprintf(stderr, "run: missing file operand");
+				fprintf(stderr, "run: missing file operand\n");
 			}
 		}
 		
-		else if (!strcmp(command, "exit") && c == '\n')	{
+		else if (!strcmp(buffer, "exit") && c == '\n')	{
 			if (c == '\n') {
 			// TODO waits until all children end, outputs result and exits app
-			fprintf(stdout, "END.");
+			fprintf(stdout, "END.\n");
 			break;
 			}
 			else {
-				fprintf(stderr, "exit: too many arguments");
+				fprintf(stderr, "exit: too many arguments\n");
 			}
 		}
 		else {
-			fprintf(stderr, "%s: command not found", command);
+			fprintf(stderr, "%s: command not found\n", buffer);
+			while (getchar() != '\n');
 		}
 	}
 	return 0;
@@ -75,6 +89,6 @@ int main(int argc, char** argv) {
 static void displayUsage (const char* appName){
 	printf("Usage: %s [MAXCHILDREN]\n", appName);
 	printf("\tMAXCHILDREN defines the limit of child processes allowed.\n");
-	printf("\tIf MAXCHILDREN equals 0, the number is unlimited.");
+	printf("\tIf MAXCHILDREN equals 0 or isn't defined, the number is unlimited.\n");
 	exit(1);
 }
