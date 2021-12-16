3ecfe82d79be1fbf7e4dfcd548815802c8b36175
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2021-12-16 18:34:38 +0000

    Remove files not from /Projeto

--- Laboratórios/LAB-02/meu_script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-echo Nesta aula vamos aprender a
-echo desenvolver os nossos primeiros scripts em bash!
--- Laboratórios/LAB-02/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for input in inputs/*.txt
-do
-	echo ==== ${input} ====
-	echo Line count: $(wc -l < "${input}")
-	./CircuitRouter-SeqSolver < "${input}" | grep "Paths routed" 
-done
